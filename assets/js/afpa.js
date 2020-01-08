9a56cada8d903c6b95c4879936a9d2e05ef11511
HazielKaos <romuald.lewandoski.59234@outlook.com>
2020-01-08 22:01:54 +0100

    Add testimonials base

--- assets/js/afpa.js
+++ assets/js/afpa.js
@@ -22,3 +22,53 @@ $(function() {
     }
 
 });
+
+$(document).ready(function(){
+
+    $('.items').slick({
+        dots: true,
+        infinite: true,
+        speed: 800,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+
+        ]
+    });
+});
+
+$('.Count').each(function () {
+    var $this = $(this);
+    jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
+        duration: 1000,
+        easing: 'swing',
+        step: function () {
+            $this.text(Math.ceil(this.Counter));
+        }
+    });
+});
